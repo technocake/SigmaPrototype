bafa1c316e9abcccf71e8ccdbeadafe47fbe0f44
kongenfloat <andreflaate2@gmail.com>
2016-08-14 20:48:30 +0200

    #17 Changed variable names, added a freemind file for example use

--- utils/converter.py
+++ utils/converter.py
@@ -14,47 +14,47 @@ def convert(file):
 
 	#Have to figure out how to delete last comma in end_file
 
-	start_file = open(file, 'r')
-	name = start_file.name
+	freemind_file = open(file, 'r')
+	name = freemind_file.name
 	name = name[:-2]
 	name = name + "json"
-	end_file = open(name, 'w')
+	json_file = open(name, 'w')
 	done = True
 
-	for line in start_file:
+	for line in freemind_file:
 		if line.startswith('<node CREATED='):
 			if line.endswith('/>'):
 				if done == False:
-					end_file.write(', ')
+					json_file.write(', ')
 					text1 = re.search('TEXT="(.+?)"', line).group()
 					text2 = re.search('"(.+?)"', text1).group()
 					line_towrite = '"resource":' + str(text2)
-					end_file.write(line_towrite)
+					json_file.write(line_towrite)
 				else:
 					text1 = re.search('TEXT="(.+?)"', line).group()
 					text2 = re.search('"(.+?)"', text1).group()
 					line_towrite = '{"resource":' + str(text2)
-					end_file.write(line_towrite)
+					json_file.write(line_towrite)
 				
 				done = False
 			
 			else:
 				if done == False:
-					end_file.write('}\n')
+					json_file.write('}\n')
 
 				text1 = re.search('TEXT="(.+?)"', line).group()
 				text2 = re.search('"(.+?)"', text1).group()
 				line_towrite = '{' + str(text2) + ':[\n'
-				end_file.write(line_towrite)
+				json_file.write(line_towrite)
 				done = True
 
 		elif line.startswith('</node>'):
 			#How to delete last comma in end_file
-			end_file.write("]},\n")
+			json_file.write("]},\n")
 
-	end_file.write("]},\n")
+	json_file.write("]},\n")
 
-	print(end_file.name + " created.")
+	print(json_file.name + " created.")
 
 convert(sys.argv[1])
 
