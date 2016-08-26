f3cd142b002b4a8cba0a21f742d73302a6218c44
technocake <technocake@komsys.org>
2016-08-26 12:44:14 +0200

    add Knut

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@
 
 # auth.py
 import sigma
-robin_invited = ["amund", "oliwia", "lars", "lasseeli", "roverelk", "fredrik", "hatland", "vegar", "Myhres"]
+robin_invited = ["amund", "oliwia", "lars", "lasseeli", "roverelk", "fredrik", "hatland", "vegar", "Myhres", "knutovsthus"]
 jonas_invited = ["kjell"]
 users = ["technocake", "jonas", "carl henrik", "andre"] + robin_invited + jonas_invited
 
