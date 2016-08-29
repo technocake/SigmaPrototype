af3f373984c99a58f5c99387d34148c8fce78701
Arxcis <jonas.solsvik@gmail.com>
2016-08-29 10:01:55 +0200

    Spawned CONFIG.js
    - First try at using CONFIG.js.
    - Works like a charm! :)

---
+++ static/js/CONFIG.js
@@ -0,0 +1,19 @@
+/* CONFIG.js */
+
+
+CONF = {
+
+    // mapssvg.html rules
+
+
+    // These two, determines how far away from their mothernode the childnodes should appear.
+    // We use relative coordinates here, so 100/100 would be 100% of the screen width.
+
+    subradx: 27/100,
+    subrady: 25/100,
+
+    urlradx: 9/100,
+    urlrady: 5/100
+
+
+}
